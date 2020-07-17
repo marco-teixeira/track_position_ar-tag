d9ff139ae2ba068663c81b420b27d428b0af471e
control-robotics <mantonio.t91@gmail.com>
2020-07-17 15:47:12 -0300

    add distance traveled

--- scripts/track_position_ARtag.py
+++ scripts/track_position_ARtag.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import ColorRGBA
 from ar_track_alvar_msgs.msg import AlvarMarkers
+from track_position_ar_tag.msg import info, infoVector
 from visualization_msgs.msg import MarkerArray
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
@@ -26,11 +27,14 @@ line_scale = rospy.get_param("/track_position_line/line_scale",0.01)
 #Global variables
 arData = AlvarMarkers()
 marker_array = MarkerArray()
+info_array = infoVector()
 ar_pose_matrix = numpy.full((len(track_ar_ids_param),int(queue_size_param)), Point())
 ar_color_rgba = numpy.full(len(track_ar_ids_param), ColorRGBA())
 matrix_position = numpy.zeros(len(track_ar_ids_param), int)
 
 pub_rviz_markerArray = rospy.Publisher(output_topic, MarkerArray, queue_size=10)
+
+pub_traveled = rospy.Publisher("/total_traveled", infoVector, queue_size=10)
 
 
 #configuring the rgb colors of the line
@@ -51,13 +55,14 @@ def set_colors():
 def ar_callback(data):
 	for point in data.markers:
 		for i in range(0,len(track_ar_ids_param)):
-			if str(point.id) == track_ar_ids_param[i]:
+			if (point.id) == int(track_ar_ids_param[i]):
 				if (matrix_position[i] < queue_size_param):
-					ar_pose_matrix[i][matrix_position[i]] = Point()
-					ar_pose_matrix[i][matrix_position[i]].x = point.pose.pose.position.x
-					ar_pose_matrix[i][matrix_position[i]].y = point.pose.pose.position.y
-					ar_pose_matrix[i][matrix_position[i]].z = point.pose.pose.position.z
-					matrix_position[i] += 1;
+				        if euclidean_distance(ar_pose_matrix[i][matrix_position[i]-1],point.pose.pose.position) > float(considered_displacement):
+					        ar_pose_matrix[i][matrix_position[i]] = Point()
+					        ar_pose_matrix[i][matrix_position[i]].x = point.pose.pose.position.x
+					        ar_pose_matrix[i][matrix_position[i]].y = point.pose.pose.position.y
+					        ar_pose_matrix[i][matrix_position[i]].z = point.pose.pose.position.z
+					        matrix_position[i] += 1;
 				else:
 					if euclidean_distance(ar_pose_matrix[i][matrix_position[i]-1],point.pose.pose.position) > float(considered_displacement):
 						ar_pose_matrix[i] = numpy.roll(ar_pose_matrix[i], -1, axis=0)
@@ -92,6 +97,18 @@ def pub_tracker():
 
 	pub_rviz_markerArray.publish(marker_array)
 
+def pub_total_traveled():
+        info_array.info_vector = []
+        for i in range(0,len(track_ar_ids_param)):
+                dist = 0
+                for j in range(1, len(ar_pose_matrix[i])-1):   
+                        dist = dist + euclidean_distance(ar_pose_matrix[i][j],ar_pose_matrix[i][j+1])
+                inf = info()
+                inf.id = int(track_ar_ids_param[i])
+                inf.dist = dist;
+                info_array.info_vector.append(inf)
+        pub_traveled.publish(info_array)
+        print (dist)
 
 
 if __name__ == '__main__':
@@ -101,6 +118,7 @@ if __name__ == '__main__':
 	set_colors()
 	while not rospy.is_shutdown():
 		pub_tracker()
+		pub_total_traveled()
 		rate.sleep()
 	
 
